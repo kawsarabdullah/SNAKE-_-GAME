748a3fbcd432f33b91f907bc786644bd95b1957a
Kawsar <abdullah15-14325@diu.edu.bd>
2023-01-07 02:59:56 +0600

    js

---
+++ index.js
@@ -0,0 +1,38 @@
+
+let board;
+let context;
+let blocksize = 20;
+let cols = 30;
+let rows = 20;
+
+let bite_food;
+let game_over;
+
+let snakeX = 0;
+let snakeY = 0;
+let tail = [];
+
+let foodX = 0;
+let foodY = 0;
+
+let score = 0;
+
+let velocityX = 1;
+let velocityY = 0;
+
+let game_over = false;
+
+window.onload = () => {}
+
+function update(){}
+
+function foodPlace(){}
+
+function changeDirection(){}
+
+function game_overEvent(){}
+
+function createRect(){}
+
+function createText(){}
+
